7256e2e6d0645a467982efca877f4d52f1754bb3
Aniket <122909119+AniketCode10@users.noreply.github.com>
2024-02-01 18:14:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const Router = require('./router')
+
+app.use('view engine','ejs')
+app.use(Router)
+
+
+app.listen(3000,()=>{
+    console.log(`Server on ${port} successully`)
+});
+
+
+
+
